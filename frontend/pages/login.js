09c68280b3d05d95c68f217121b13c0c4951d732
novalfikriyansyah-max <novalfikriyansyah@gmail.com>
2025-11-06 23:57:32 +0900

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -6,7 +6,7 @@ export default function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch("https://your-backend.onrender.com/api/auth/login", {
+    const res = await fetch("https://finance-track-pleg.onrender.com/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
